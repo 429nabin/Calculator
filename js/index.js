1e1aaaa1ca2b553f3bab7f2692215a1abc3b384e
Nabin Karki <429nabin@users.noreply.github.com>
2022-05-28 09:44:36 +1000

    added worng js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
-$(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
-    });
-  });
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operator = undefined;
+  }
+  delete() {
+
+  }
+  appendNumber(numbers) {
+    this.currentOperand = numbers;
+  }
+  chooseOperation(operator) {}
+  compute() {}
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-numbers]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-allClear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previousDisplay]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-currentDisplay]"
+);
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay();
+  })
+})
